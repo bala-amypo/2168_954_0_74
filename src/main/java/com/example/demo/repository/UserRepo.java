32bc61cb390f6c5ca0760789f08d81c84f7c7ce6
bala-amypo <bala@amypo.in>
2025-12-18 08:32:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.UserEntity;
 
 public interface UserRepo extends JpaRepository<UserEntity, Long> {
-    // You can add custom queries here if needed
+    
 }
--- src/main/java/com/example/demo/repository/VehicleRepo.java
+++ src/main/java/com/example/demo/repository/VehicleRepo.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.VehicleEntity;
 
 public interface VehicleRepo extends JpaRepository<VehicleEntity, Long> {
-    // custom queries if needed
+
 }
